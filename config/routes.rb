b6371417198deee7904568446f458a202512bd36
William Mathewson <wncmathewson@me.com>
2014-01-20 12:29:34 +0000

    Added a comment to remind whomever how routes work

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Stack::Application.routes.draw do
 
+  # The routes file matches our URLs up
+  # with our controllers
+
   # I want to add in all the REST urls for the stories
   # e.g. index, show, new , create
   # edit, update, destroy
