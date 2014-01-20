734bce4b70261f86e747dd30223c5404c439bcdc
William Mathewson <wncmathewson@me.com>
2014-01-20 12:49:56 +0000

    Started adding actions to the Stories controller

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,2 +1,9 @@
 class StoriesController < ApplicationController
+
+  # Controllers manage the urls,
+  # the data from the models, and our HTML views
+
+  def index
+    @username = 'willmatty'
+  end
 end
